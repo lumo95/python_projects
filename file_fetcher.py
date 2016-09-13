316bc27d386a0e7ff021c26aa9120ffaf60d0da0
lumo95 <iluke95@hotmail.com>
2016-09-13 19:01:04 +0100

    Update file_fetcher.py

--- file_fetcher.py
+++ file_fetcher.py
@@ -5,20 +5,20 @@
 
 from urllib import request
 
-goog_url = 'http://www.textfiles.com/100/914bbs.txt'
+file_url = 'http://www.textfiles.com/100/914bbs.txt'
 
 
-def stock_data(csv_url):
+def get_file(csv_url):
     response = request.urlopen(csv_url)  # storing the connection
     csv = response.read()  # reads the data from the url
     csv_string = str(csv)  # storing the data in a string
     new_line = csv_string.split("\\n")  # splits up the string to make it more readable
-    save_location = r'new_file.txt'  # r makes the string a raw string (allows explicit chars)
+    save_location = r'new_file.txt' 
     fw = open(save_location, 'w')
 
     for line in new_line:
-        fw.write(line + "\n")  # at the end of every line add a new line
+        fw.write(line + "\n") 
     fw.close()
 
-stock_data(goog_url)
+get_file(file_url)
 
